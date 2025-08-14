ac9b39a8b50583c12982c669809f4229445e1d43
Hamilton <hamshan@tbdir.net>
2025-08-14 08:53:52 -0700

    Use VITE_SERVER_URL in build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 }
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_SERVER_URL, // For local dev proxy if needed
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
